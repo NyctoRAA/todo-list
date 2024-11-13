56eb928ab088b4be5ae6916a87a7be3a2da65597
Rodrigo Amaral <ra@unknownaa0d12b56eef.lan>
2024-11-13 21:58:20 +0000

    Starting the JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 
-class Task 
---
+++ src/task.js
@@ -0,0 +1,8 @@
+class Task {
+    constructor(title, description, priority, dueDate) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+}
